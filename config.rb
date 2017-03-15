b0ae20a95a0819f13e6eb6a476cf573bcd98bae8
Phill MV <phillmv@okayfail.com>
2017-03-15 13:27:07 -0400

    changed config.rb for new template

--- config.rb
+++ config.rb
@@ -1,18 +1,79 @@
 # coding: utf-8
+
 ###
-# middleman-casper configuration
+# Page options, layouts, aliases and proxies
 ###
 
-config[:casper] = {
-  blog: {
-    url: 'http://blog.appcanary.com',
-    name: 'Appcanary',
-    description: '<a href="https://appcanary.com">Appcanary</a> makes sure you never run vulnerable software on your servers.<br/>Like our blog? Subscribe to our <a href="https://appcanary.us10.list-manage.com/subscribe?u=303b378f377508300c0b5469a&id=dc58e8f56f">newsletter</a>!',
-    date_format: '%d %B %Y',
-    navigation: false,
-    logo: 'appcanary.png' # Optional
-  },
-  authors: {
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  blog.permalink = "{year}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+set :site_url, "https://blog.appcanary.com"
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true, footnotes: true, tables: true, with_toc_data: true
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+end
+
+config[:authors] = {
     "phillmv" => {
       name: 'Phillip Mendonça-Vieira',
       bio: nil, # Optional
@@ -37,61 +98,11 @@ config[:casper] = {
       gravatar_email: nil,
       twitter: nil
     }
-  },
-  navigation: {
-    "Home" => "/",
-    "Archive" => "archive.html",
-    "Github" => "https://github.com/appcanary"
-  }
 }
 
-###
-# Page options, layouts, aliases and proxies
-###
 
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-def get_tags(resource)
-  if resource.data.tags.is_a? String
-    resource.data.tags.split(',').map(&:strip)
-  else
-    resource.data.tags
-  end
-end
-
-def group_lookup(resource, sum)
-  results = Array(get_tags(resource)).map(&:to_s).map(&:to_sym)
-
-  results.each do |k|
-    sum[k] ||= []
-    sum[k] << resource
-  end
-end
-
-tags = resources
-  .select { |resource| resource.data.tags }
-  .each_with_object({}, &method(:group_lookup))
-
-tags.each do |tag, articles|
-  proxy "/tag/#{tag.downcase.to_s.parameterize}/feed.xml", '/feed.xml',
-    locals: { tag: tag, articles: articles[0..5] }, layout: false
-end
-
-proxy "/rss", "/feed.xml"
 ready do
-  config.casper[:authors].each do |k, author|
+  config.authors.each do |k, author|
     proxy "/author/#{author[:name].parameterize}.html",
           '/author.html', ignore: true, :locals => { "page_type" => "author",
                                                      "articles" => sitemap.resources.select { |r| r.data.author == k }.sort_by { |r| r.data.date}.reverse,
@@ -99,40 +110,9 @@ ready do
   end
 end
 
-# General configuration
-# Reload the browser automatically whenever files change
-configure :development do
-  activate :livereload
-end
 
-###
-# Helpers
-###
 
-activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  blog.permalink = "{year}/{title}.html"
-  # Matcher for blog source files
-  blog.sources = "articles/{year}-{month}-{day}-{title}.html"
-  blog.taglink = "tag/{tag}.html"
-  # blog.layout = "layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
-
-  blog.tag_template = "tag.html"
-  # blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-end
+proxy "/rss", "/feed.xml"
 
 redirect 'post/131974777156/hello-appcanary', to: '/2015/hello-appcanary.html'
 redirect 'post/125101503771/hello-world', to: '/2015/hello-world.html'
@@ -143,45 +123,6 @@ redirect '2016/vikhal-morris.html', to: '/2016/tale-of-two-worms.html'
 
 # Middleman-Syntax - https://github.com/middleman/middleman-syntax
 set :haml, { ugly: true }
-set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true, smartypants: true, footnotes: true, tables: true, with_toc_data: true
 activate :syntax, line_numbers: false
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-set :relative_links, true
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use absolute URLs
-  # activate :asset_host, :host => '//blog.appcanary.com'
-
-  # Ignoring Files
-  ignore 'javascripts/_*'
-  ignore 'javascripts/vendor/*'
-  ignore 'stylesheets/_*'
-  ignore 'stylesheets/vendor/*'
-end
-
-helpers do
-  # thanks, https://forum.middlemanapp.com/t/generate-inpage-navigation-nice-api-documentation/1002/2
-  def table_of_contents(resource)
-    content = File.read(resource.source_file)
-    toc_renderer = Redcarpet::Render::HTML_TOC.new
-    markdown = Redcarpet::Markdown.new(toc_renderer, nesting_level: 2) # nesting_level is optional
-    markdown.render(content)
-  end
-end
+activate :sprockets
